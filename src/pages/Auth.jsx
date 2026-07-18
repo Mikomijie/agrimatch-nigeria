bcf0ef9bb3ebf96758afdfebac02c3b99eed9c66
Mikomijie <omijiemichael435@gmail.com>
2026-07-18 12:53:26 -0700

    add all the 36 states in nigeria

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -10,7 +10,14 @@ const ROLES = [
   { id: 'transporter', label: 'Transporter' }
 ]
 
-const REGIONS = ['Lagos', 'Kano', 'Kaduna', 'Plateau', 'Edo', 'Osun', 'Benue', 'FCT Abuja', 'Rivers', 'Enugu', 'Bauchi', 'Katsina']
+const REGIONS = [
+  'Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi', 'Bayelsa', 'Benue', 
+  'Borno', 'Cross River', 'Delta', 'Ebonyi', 'Edo', 'Ekiti', 'Enugu', 
+  'FCT Abuja', 'Gombe', 'Imo', 'Jigawa', 'Kaduna', 'Kano', 'Katsina', 
+  'Kebbi', 'Kogi', 'Kwara', 'Lagos', 'Nasarawa', 'Niger', 'Ogun', 
+  'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba', 
+  'Yobe', 'Zamfara'
+]
 
 function Auth() {
   const navigate = useNavigate()
